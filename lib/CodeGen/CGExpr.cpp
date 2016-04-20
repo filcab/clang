07f628b361024609d846d3bdc45f87be50943804
Filipe Cabecinhas <me@filcab.net>
2016-04-20 12:16:28 +0100

    [ubsan] Minimize size of data for type_mismatch

--- lib/CodeGen/CGExpr.cpp
+++ lib/CodeGen/CGExpr.cpp
@@ -576,10 +576,12 @@ void CodeGenFunction::EmitTypeCheck(TypeCheckKind TCK, SourceLocation Loc,
   }
 
   if (Checks.size() > 0) {
+    // Make sure we're not losing information. Alignment needs to be a power of 2
+    assert(!AlignVal || (uint64_t)1 << llvm::Log2_64(AlignVal) == AlignVal);
     llvm::Constant *StaticData[] = {
      EmitCheckSourceLocation(Loc),
       EmitCheckTypeDescriptor(Ty),
-      llvm::ConstantInt::get(SizeTy, AlignVal),
+      llvm::ConstantInt::get(Int8Ty, llvm::Log2_64(AlignVal)),
       llvm::ConstantInt::get(Int8Ty, TCK)
     };
     EmitCheck(Checks, "type_mismatch", StaticData, Ptr);
